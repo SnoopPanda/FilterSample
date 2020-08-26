34bdec1c0bcaffe6df5c7e488a790ceaa87baa13
WangJie <wangjie@raintai.com>
2020-08-26 11:37:18 +0800

    first commit

---
+++ FilterSample/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FilterSample
+//
+//  Created by 王杰 on 2020/8/26.
+//  Copyright © 2020 SPPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
