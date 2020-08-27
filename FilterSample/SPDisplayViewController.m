a6230f5b98a4d2871c9919d3fb41d46a30598fe7
WangJie <wangjie@raintai.com>
2020-08-27 13:52:29 +0800

    add some filter

--- FilterSample/SPDisplayViewController.m
+++ FilterSample/SPDisplayViewController.m
@@ -112,6 +112,140 @@
          self.slider.value = 1;
      }
     
+    if ([_title isEqualToString:@"反色"]) {
+         if (!self.filter) {
+             self.filter = [[GPUImageColorInvertFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+         self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"褐色(怀旧)"]) {
+         if (!self.filter) {
+             self.filter = [[GPUImageSepiaFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+         self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"色阶"]) {
+         if (!self.filter) {
+             self.filter = [[GPUImageLevelsFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+         self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"灰度"]) {
+          if (!self.filter) {
+              self.filter = [[GPUImageGrayscaleFilter alloc] init];
+               [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+               [self.sourcePicture addTarget:self.filter];
+               [self.filter addTarget:self.imageView];
+          }
+          self.slider.hidden = YES;
+      }
+
+    
+    if ([_title isEqualToString:@"色彩直方图"]) { // TODO
+         _maximumValue = 16;
+         _minimumValue = 1;
+         if (!self.filter) {
+             self.filter = [[GPUImageHistogramFilter alloc] initWithHistogramType:kGPUImageHistogramRed];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+//        ((GPUImageHistogramFilter *)self.filter).downsamplingFactor = 16;
+        self.slider.value = 16;
+     }
+    
+    if ([_title isEqualToString:@"RGB"]) {
+         _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageRGBFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImageRGBFilter *)self.filter).red = 1;
+        ((GPUImageRGBFilter *)self.filter).green = 1;
+        ((GPUImageRGBFilter *)self.filter).blue = 1;
+        self.slider.value = 1;
+     }
+    
+    if ([_title isEqualToString:@"色调曲线"]) {
+         if (!self.filter) {
+             self.filter = [[GPUImageToneCurveFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"单色"]) {
+         _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageMonochromeFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+//        ((GPUImageMonochromeFilter *)self.filter).red = 1;
+        self.slider.value = 1;
+     }
+    
+    if ([_title isEqualToString:@"不透明度"]) {
+         _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageOpacityFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImageOpacityFilter *)self.filter).opacity = 1;
+        self.slider.value = 1;
+     }
+    
+    if ([_title isEqualToString:@"提亮阴影"]) {
+         _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageHighlightShadowFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImageHighlightShadowFilter *)self.filter).shadows = 0;
+        self.slider.value = 0;
+     }
+    
+    if ([_title isEqualToString:@"提亮阴影"]) {
+         _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageFalseColorFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImageFalseColorFilter *)self.filter).shadows = 0;
+        self.slider.value = 0;
+     }
+    
+    
+    
+
     if ([_title isEqualToString:@"素描"]) {
          if (!self.filter) {
              self.filter = [[GPUImageSketchFilter alloc] init];
@@ -284,7 +418,102 @@
         self.slider.value = 0.25;
      }
     
+    if ([_title isEqualToString:@"球型折射"]) {
+        _maximumValue = 1;
+         _minimumValue = 0;
+         if (!self.filter) {
+             self.filter = [[GPUImageSphereRefractionFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImageSphereRefractionFilter *)self.filter).radius = 0.25;
+        self.slider.value = 0.25;
+     }
     
+    if ([_title isEqualToString:@"色调分离"]) {
+        _maximumValue = 256;
+         _minimumValue = 1;
+         if (!self.filter) {
+             self.filter = [[GPUImagePosterizeFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        ((GPUImagePosterizeFilter *)self.filter).colorLevels = 10;
+        self.slider.value = 10;
+     }
+    
+    if ([_title isEqualToString:@"CGA色彩滤镜"]) {
+         if (!self.filter) {
+             self.filter = [[GPUImageCGAColorspaceFilter alloc] init];
+              [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+              [self.sourcePicture addTarget:self.filter];
+              [self.filter addTarget:self.imageView];
+         }
+        self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"柏林燥点"]) {
+         _maximumValue = 1;
+          _minimumValue = -1;
+          if (!self.filter) {
+              self.filter = [[GPUImagePerlinNoiseFilter alloc] init];
+               [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+               [self.sourcePicture addTarget:self.filter];
+               [self.filter addTarget:self.imageView];
+          }
+        ((GPUImagePerlinNoiseFilter *)self.filter).scale = 0;
+        self.slider.value = 0;
+     }
+    
+    if ([_title isEqualToString:@"3x3卷积"]) {
+          if (!self.filter) {
+              self.filter = [[GPUImage3x3ConvolutionFilter alloc] init];
+               [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+               [self.sourcePicture addTarget:self.filter];
+               [self.filter addTarget:self.imageView];
+          }
+        self.slider.hidden = YES;
+     }
+    
+    if ([_title isEqualToString:@"浮雕"]) {
+        _maximumValue = 4;
+        _minimumValue = 0;
+        if (!self.filter) {
+            self.filter = [[GPUImageEmbossFilter alloc] init];
+             [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+             [self.sourcePicture addTarget:self.filter];
+             [self.filter addTarget:self.imageView];
+        }
+        ((GPUImageEmbossFilter *)self.filter).intensity = 1;
+        self.slider.value = 1;
+     }
+    
+    if ([_title isEqualToString:@"像素圆点花样"]) {
+        _maximumValue = 1;
+        _minimumValue = 0;
+        if (!self.filter) {
+            self.filter = [[GPUImagePolkaDotFilter alloc] init];
+             [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+             [self.sourcePicture addTarget:self.filter];
+             [self.filter addTarget:self.imageView];
+        }
+        ((GPUImagePolkaDotFilter *)self.filter).dotScaling = 1;
+        self.slider.value = 1;
+     }
+    
+    if ([_title isEqualToString:@"点染"]) {
+
+        if (!self.filter) {
+            self.filter = [[GPUImageHalftoneFilter alloc] init];
+             [self.filter forceProcessingAtSize:self.imageView.sizeInPixels];
+             [self.sourcePicture addTarget:self.filter];
+             [self.filter addTarget:self.imageView];
+        }
+        self.slider.hidden = YES;
+     }
+
     [self.sourcePicture processImage];
 }
 
@@ -311,6 +540,24 @@
          ((GPUImageGammaFilter *)self.filter).gamma = value;
      }
     
+    if ([_title isEqualToString:@"色彩直方图"]) {
+        ((GPUImageHistogramFilter *)self.filter).downsamplingFactor = value;
+     }
+    
+    if ([_title isEqualToString:@"RGB"]) {
+        ((GPUImageRGBFilter *)self.filter).red = value;
+        ((GPUImageRGBFilter *)self.filter).green = value;
+        ((GPUImageRGBFilter *)self.filter).blue = value;
+     }
+    
+    if ([_title isEqualToString:@"不透明度"]) {
+        ((GPUImageOpacityFilter *)self.filter).opacity = value;
+     }
+    
+    if ([_title isEqualToString:@"提亮阴影"]) {
+        ((GPUImageHighlightShadowFilter *)self.filter).shadows = value;
+     }
+    
     if ([_title isEqualToString:@"卡通"]) {
         ((GPUImageToonFilter *)self.filter).threshold = value;
      }
@@ -325,7 +572,7 @@
     
     
     if ([_title isEqualToString:@"马赛克"]) {
-        ((GPUImageMosaicFilter *)self.filter).numTiles = self.slider.value;
+        ((GPUImageMosaicFilter *)self.filter).numTiles = value;
      }
     
     if ([_title isEqualToString:@"像素化"]) {
@@ -333,33 +580,54 @@
      }
     
     if ([_title isEqualToString:@"同心圆像素化"]) {
-        ((GPUImagePolarPixellateFilter *)self.filter).pixelSize = CGSizeMake(self.slider.value, self.slider.value);
+        ((GPUImagePolarPixellateFilter *)self.filter).pixelSize = CGSizeMake(value, value);
      }
     
     if ([_title isEqualToString:@"交叉线阴影"]) {
-        ((GPUImageCrosshatchFilter *)self.filter).crossHatchSpacing = self.slider.value;
+        ((GPUImageCrosshatchFilter *)self.filter).crossHatchSpacing = value;
      }
     
     if ([_title isEqualToString:@"晕影"]) {
-        ((GPUImageVignetteFilter *)self.filter).vignetteStart = self.slider.value;
+        ((GPUImageVignetteFilter *)self.filter).vignetteStart = value;
      }
     
     
     if ([_title isEqualToString:@"漩涡"]) {
-        ((GPUImageSwirlFilter *)self.filter).radius = self.slider.value;
+        ((GPUImageSwirlFilter *)self.filter).radius = value;
      }
     
     if ([_title isEqualToString:@"凸面镜"]) {
-        ((GPUImageBulgeDistortionFilter *)self.filter).radius = self.slider.value;
+        ((GPUImageBulgeDistortionFilter *)self.filter).radius = value;
      }
     
     
     if ([_title isEqualToString:@"凹面镜"]) {
-        ((GPUImagePinchDistortionFilter *)self.filter).radius = self.slider.value;
+        ((GPUImagePinchDistortionFilter *)self.filter).radius = value;
      }
     
     if ([_title isEqualToString:@"水晶球"]) {
-        ((GPUImageGlassSphereFilter *)self.filter).radius = self.slider.value;
+        ((GPUImageGlassSphereFilter *)self.filter).radius = value;
+     }
+    
+    if ([_title isEqualToString:@"球型折射"]) {
+        ((GPUImageSphereRefractionFilter *)self.filter).radius = value;
+     }
+    
+    if ([_title isEqualToString:@"色调分离"]) {
+        ((GPUImagePosterizeFilter *)self.filter).colorLevels = value;
+     }
+    
+    
+    if ([_title isEqualToString:@"柏林燥点"]) {
+        ((GPUImagePerlinNoiseFilter *)self.filter).scale = value;
+     }
+    
+    if ([_title isEqualToString:@"浮雕"]) {
+        ((GPUImageEmbossFilter *)self.filter).intensity = value;
+     }
+    
+    if ([_title isEqualToString:@"像素圆点花样"]) {
+        ((GPUImagePolkaDotFilter *)self.filter).dotScaling = value;
      }
     
     [self.sourcePicture processImage];
